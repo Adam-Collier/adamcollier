5296b00e5ff243b8704b47fecd1dc3ccf3b0a9d8
Adam-Collier <adamcollierart@gmail.com>
2021-12-24 18:31:13 +0000

    get latest films from letterboxd

--- app/home.ts
+++ app/home.ts
@@ -21,3 +21,27 @@ export const getBooks = async () => {
 
   return { read, toRead, reading: reading[0] };
 };
+
+export const getLatestFilms = async () => {
+  const parser = new Parser();
+  // the exclamation mark here tells typescript that the values will never be null
+  const feed = await parser.parseURL(process.env.LETTERBOXD_LATEST!);
+  const { items } = feed;
+
+  const filteredFilms = items.slice(0, 5).map((film) => {
+    const { content, title } = film;
+
+    const src = content?.split('"')[1];
+    const [filmTitle, ...rest] = title?.split(",")!;
+    const [year, rating] = rest.join("").split("-");
+
+    return {
+      title: filmTitle,
+      year,
+      rating,
+      src,
+    };
+  });
+  // limit to what we need
+  return filteredFilms;
+};
