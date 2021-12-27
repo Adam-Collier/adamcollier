645d746d7444f0960327c75e2ea4edde900b988e
Adam-Collier <adamcollierart@gmail.com>
2021-12-27 12:28:23 +0000

    only send the data we need to use

--- app/home.ts
+++ app/home.ts
@@ -1,44 +1,62 @@
 import Parser from "rss-parser";
 
 export const getBooks = async () => {
-  const urls = [
-    process.env.BOOKS_READ,
-    process.env.BOOKS_WANT_TO_READ,
-    process.env.BOOKS_CURRENTLY_READING,
-  ];
+  const urls = [process.env.BOOKS_READ, process.env.BOOKS_CURRENTLY_READING];
 
   const parser = new Parser();
 
-  const [read, toRead, reading] = await Promise.all(
+  const [read, reading] = await Promise.all(
     urls.map(async (url) => {
       // the exclamation mark here tells typescript that the values will never be null
       const feed = await parser.parseURL(url!);
       const { items } = feed;
       // limit to what we need
-      return items.slice(0, 3);
+      const books = items.slice(0, 3);
+
+      const filteredPropertied = books.map(
+        ({
+          link,
+          title,
+          creator,
+        }): {
+          link: string | undefined;
+          title: string | undefined;
+          creator: string | undefined;
+        } => ({
+          link,
+          title,
+          creator,
+        })
+      );
+
+      return filteredPropertied;
     })
   );
 
-  return { read, toRead, reading: reading[0] };
+  return { read, reading: reading[0] };
 };
 
 export const getLatestFilms = async () => {
   const parser = new Parser();
-  // the exclamation mark here tells typescript that the values will never be null
+
   const feed = await parser.parseURL(process.env.LETTERBOXD_LATEST!);
   const { items } = feed;
 
   const filteredFilms = items.slice(0, 5).map((film) => {
-    const { content, title } = film;
+    const { content, title, link } = film;
 
     const src = content?.split('"')[1];
-    const [filmTitle, ...rest] = title?.split(",")!;
-    const [year, rating] = rest.join("").split("-");
+
+    let lastCommaIndex = title?.lastIndexOf(",");
+    const filmTitle = title?.substring(0, lastCommaIndex);
+    const rest = title?.substring(lastCommaIndex!);
+    const [year, rating] = rest?.split("-")!;
 
     return {
+      link,
       title: filmTitle,
-      year,
-      rating,
+      year: year.trim(),
+      rating: rating.trim(),
       src,
     };
   });
