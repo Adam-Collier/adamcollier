703cd2e3061c564209c41e9b4b3c62ed55410ece
Adam-Collier <adamcollierart@gmail.com>
2021-12-30 18:02:10 +0000

    custom variant for child hrefs

--- unocss.config.ts
+++ unocss.config.ts
@@ -6,5 +6,16 @@ export default defineConfig({
       btn: "bg-neutral-900 hover:bg-neutral-700 text-white py-2 px-4 rounded mt-2",
     },
   ],
+  variants: [
+    // href: hover:
+    (matcher) => {
+      if (!matcher.startsWith("href:")) return matcher;
+      return {
+        // slice `hover:` prefix and passed to the next variants and rules
+        matcher: matcher.slice(5),
+        selector: (s) => `${s} a`,
+      };
+    },
+  ],
   presets: [presetUno(), presetIcons()],
 });
