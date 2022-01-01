78d3645102d7ec48afbae96d865e76d6da4e69e2
Adam-Collier <adamcollierart@gmail.com>
2022-01-01 20:35:17 +0000

    cache-control, smaller text and fix active link issue

--- app/routes/inspiration.tsx
+++ app/routes/inspiration.tsx
@@ -3,22 +3,27 @@ import { NavLinkProps } from "remix"
 
 
 const Link = (props: NavLinkProps) => (
-  <NavLink {...props} className={({ isActive }) => isActive ? "bg-gray-200 px-2 py-1 rounded" : "px-2 py-1 rounded"} />
+  <NavLink {...props} className={({ isActive }) => isActive ? "bg-gray-200 px-2 py-1 rounded text-sm" : "px-2 py-1 rounded text-sm"} />
 )
 
-const Inspiration = () => {
+export function headers({ }) {
+  return {
+    "Cache-Control": "s-maxage=1, stale-while-revalidate"
+  };
+}
 
-  return <main>
+const Inspiration = () => (
+  <main>
     <Outlet />
     <nav className="fixed shadow-md bottom-24 left-8">
       <ul className="flex px-2 py-2 bg-gray-100 rounded children:cursor-pointer">
-        <li><Link to="/inspiration">All</Link></li>
+        <li><Link to="/inspiration/">All</Link></li>
         <li><Link to="/inspiration/desktop">Desktop</Link></li>
         <li><Link to="/inspiration/print">Print</Link></li>
         <li><Link to="/inspiration/3D">3D</Link></li>
       </ul>
     </nav>
   </main>
-}
+)
 
 export default Inspiration
