37f4cb96f792a3eef30d5db25b701d02005e5004
Adam-Collier <adamcollierart@gmail.com>
2022-01-01 20:35:50 +0000

    sub pages now work

---
+++ app/routes/inspiration/$slug.tsx
@@ -0,0 +1,24 @@
+import { json, LoaderFunction, useLoaderData } from "remix";
+import Masonry from "~/components/Masonry";
+import { saveeBoards } from "./index";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { slug } = params;
+
+  if (!slug) throw new Response("Not Found", { status: 404 });
+  const id = saveeBoards[slug];
+  const response = await fetch(
+    `https://api.savee.it/user/adamcollier/boards/${id}/items/`
+  );
+  const { data } = await response.json();
+  return json(data);
+}
+
+const InspirationPage = () => {
+  const data = useLoaderData();
+  return <main>
+    <Masonry images={data} />
+  </main>
+}
+
+export default InspirationPage;
