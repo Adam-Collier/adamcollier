1f57e2062e0c50db1abac649d50816a9299a41d3
Adam-Collier <adamcollierart@gmail.com>
2022-01-02 19:41:04 +0000

    snippets layout and menu

---
+++ app/routes/snippets.tsx
@@ -0,0 +1,45 @@
+import { json, Link, LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+import { toSlug } from "~/utils/utils";
+
+export const loader: LoaderFunction = async () => {
+  // get everything we need for the headings here
+  const data = await db.snippetCollection.findMany({
+    select: {
+      name: true,
+      snippets: {
+        select: {
+          title: true
+        }
+      }
+    }
+  });
+
+  return json(data);
+}
+
+const Snippets = () => {
+  const data = useLoaderData();
+
+  return (<div className="flex flex-col sm:flex-row gap-8 md:gap-16 block max-w-4xl mx-auto px-4 py-16">
+    <aside>
+      <ul className="flex flex-col gap-1">
+        {data.map(({ name, snippets }: { name: string, snippets: [{ title: string }] }) => (
+          <>
+            <li className="font-medium"><Link to={`/snippets/${toSlug(name)}`}>{name}</Link></li>
+            <ul className="flex flex-col gap-0.5 pl-2">
+              {snippets && snippets.map(({ title }) => (
+                <li className="text-sm hover:underline text-gray-500"><Link to={`/snippets/${toSlug(name)}#${toSlug(title)}`}>{title}</Link></li>
+              ))}
+            </ul>
+          </>
+        ))}
+      </ul>
+    </aside>
+    <div>
+
+    </div>
+  </div>)
+}
+
+export default Snippets;
