fcc70f718531270ff44315acb8b08c864cf47afa
Adam-Collier <adamcollierart@gmail.com>
2022-01-11 21:47:21 +0000

    fixed authentication

--- app/routes/admin/music/new.tsx
+++ app/routes/admin/music/new.tsx
@@ -1,7 +1,12 @@
-import { Link, Outlet } from "remix"
+import { Link, LoaderFunction, Outlet } from "remix"
+import { getUser } from "~/utils/session.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const isAuthenticated = await getUser(request)
+  if (!isAuthenticated) throw new Response("Unauthorized", { status: 401 });
+}
 
 const NewMusic = () => {
-
   return (
     <main className="flex flex-col sm:flex-row gap-8 md:gap-16 block max-w-4xl mx-auto px-4">
       <div className="md:flex-shrink-0 pt-16">
