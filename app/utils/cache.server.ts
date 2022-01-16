b38a36af1d57b661192ada1f6f49fba2b033c713
Adam Collier <adamcollierart@gmail.com>
2022-01-16 17:12:38 +0000

    lru-cache

--- app/utils/cache.server.ts
+++ app/utils/cache.server.ts
@@ -2,12 +2,17 @@ import LRU from 'lru-cache'
 
 const createLruCache = () => {
   // doing anything other than any was a pain
-  const newCache = new LRU<string, { value: any }>({
+  const newCache = new LRU<string, any>({
     max: 1000,
     maxAge: 1000 * 60 * 60, // 1 hour
   })
 
   return newCache
+}
+declare global {
+  // This preserves the LRU cache during development
+  // eslint-disable-next-line
+  var lruCache: LRU<string, { value: any }> | undefined
 }
 
 const lruCache = (global.lruCache = global.lruCache
