716a4d58b386852fc4223b4e29fe86a0f9d62884
Adam <adamcollierart@gmail.com>
2022-01-17 19:38:01 +0000

    Snippets node (#9)
    * downgrade packages to use require()
    
    * generate correct html

--- app/utils/utils.server.ts
+++ app/utils/utils.server.ts
@@ -1,30 +1,31 @@
-export const toHTML = async (markdown: string) => {
-  const { unified } = await import('unified')
-  const { default: remarkParse } = await import('remark-parse')
-  const { default: remarkRehype } = await import('remark-rehype')
-  const { default: rehypeExternalLinks } = await import('rehype-external-links')
-  const { default: rehypeSanitize } = await import('rehype-sanitize')
-  const { default: rehypeStringify } = await import('rehype-stringify')
+const unified = require('unified')
 
+const remarkParse = require('remark-parse')
+const remarkRehype = require('remark-rehype')
+const remarkStringify = require('remark-stringify')
+const remarkExternalLinks = require('remark-external-links')
+
+const rehypeStringify = require('rehype-stringify')
+const rehypeRemark = require('rehype-remark')
+const rehypeParse = require('rehype-parse')
+
+const highlight = require('~/utils/unified/highlight')
+
+export const toHTML = async (markdown: string) => {
   let processor = unified()
     .use(remarkParse) // Parse markdown content to a syntax tree
     .use(remarkRehype) // Turn markdown syntax tree to HTML syntax tree, ignoring embedded HTML
-    .use(rehypeExternalLinks)
-    .use(rehypeSanitize)
-    .use(rehypeStringify) // Serialize HTML syntax tree
+    .use(await highlight)
+    .use(remarkExternalLinks)
+    .use(rehypeStringify)
 
   let html = await processor.process(markdown)
 
   // make sure to return a string otherwise Typescript will throw an error regarding VFile
-  return html.value.toString()
+  return html.contents
 }
 
 export const toMarkdown = async (html: string) => {
-  const { unified } = await import('unified')
-  const { default: rehypeParse } = await import('rehype-parse')
-  const { default: rehypeRemark } = await import('rehype-remark/index.js')
-  const { default: remarkStringify } = await import('remark-stringify')
-
   const processor = unified()
     .use(rehypeParse)
     .use(rehypeRemark)
