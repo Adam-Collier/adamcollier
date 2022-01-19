14d68eecc5df8e1502882f7a3b964db084af59a8
Adam-Collier <adamcollierart@gmail.com>
2022-01-19 22:16:28 +0000

    better positioning on desktop

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,44 +14,46 @@ export const Header = () => {
   const { user } = useAuth()
 
   return (
-    <ul className="flex flex-row justify-around items-center p-4 space-x-4 rounded-md fixed bottom-4 w-fit shadow-md z-10 bg-gray-200 bg-opacity-70 backdrop-filter backdrop-blur-sm left-1/2 -translate-x-1/2">
-      <li>
-        <Link to="/">
-          <Home />
-        </Link>
-      </li>
-      <li>
-        <Link to="/posts">
-          <Posts />
-        </Link>
-      </li>
-      <li>
-        <Link to="/snippets">
-          <Snippets />
-        </Link>
-      </li>
-      <li>
-        <Link to="/resources">
-          <Resources />
-        </Link>
-      </li>
-      <li>
-        <Link to="/inspiration">
-          <Inspiration />
-        </Link>
-      </li>
-      <li>
-        <Link to="/music">
-          <Music />
-        </Link>
-      </li>
-      {user && (
-        <form action="/logout" method="post">
-          <button className="block" type="submit">
-            <Signout />
-          </button>
-        </form>
-      )}
-    </ul>
+    <div className="flex fixed bottom-4 max-w-5xl w-full mx-auto left-1/2 -translate-x-1/2 justify-center sm:justify-start px-4">
+      <ul className="flex justify-around items-center p-4 space-x-4 rounded-md w-fit shadow-md z-10 bg-gray-200 bg-opacity-70 backdrop-filter backdrop-blur-sm">
+        <li>
+          <Link to="/">
+            <Home />
+          </Link>
+        </li>
+        <li>
+          <Link to="/posts">
+            <Posts />
+          </Link>
+        </li>
+        <li>
+          <Link to="/snippets">
+            <Snippets />
+          </Link>
+        </li>
+        <li>
+          <Link to="/resources">
+            <Resources />
+          </Link>
+        </li>
+        <li>
+          <Link to="/inspiration">
+            <Inspiration />
+          </Link>
+        </li>
+        <li>
+          <Link to="/music">
+            <Music />
+          </Link>
+        </li>
+        {user && (
+          <form action="/logout" method="post">
+            <button className="block" type="submit">
+              <Signout />
+            </button>
+          </form>
+        )}
+      </ul>
+    </div>
   )
 }
