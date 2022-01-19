79c3dcbafd0070c9c583efa8e0961d3c24ccd89f
Adam Collier <adamcollierart@gmail.com>
2022-01-19 18:56:19 +0000

    AdminToolbar for resource and snippets parent routes

--- app/routes/resources/$slug.tsx
+++ app/routes/resources/$slug.tsx
@@ -4,11 +4,12 @@ import { toTitleCase } from '~/utils/utils'
 import { Resource as ResourceProps } from '@prisma/client'
 import Resource from '~/components/Resource'
 import { useAuth } from '~/context'
+import AdminToolbar from '~/components/AdminToolbar'
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params
 
-  if (!slug) return
+  if (!slug) return null
 
   const data = await db.resourceCollection.findUnique({
     where: { name: toTitleCase(slug) },
@@ -30,35 +31,35 @@ const Resources = () => {
   const data = useLoaderData()
   const { user } = useAuth()
 
-  const { name, description, resources } = data
+  const { name, description, resources, id } = data
 
   let sectionName = ''
 
   return (
-    <div className="flex flex-col gap-4">
-      {user && (
-        <Link to="/admin/resources/new" className="hover:underline">
-          <i className="text-sm flex items-center gap-1 hover:underline">
-            Add New Resource{' '}
-            <span className="inline-block i-ri:arrow-right-line" />
-          </i>
+    <>
+      <AdminToolbar user={user}>
+        <Link to={`/admin/resources/collection/edit/${id}`}>
+          Edit Collection
         </Link>
-      )}
-      <h1 className="text-2xl">{name}</h1>
-      <p>{description}</p>
-      {resources.map((props: ResourceProps) => {
-        const resource = (
-          <>
-            {props.section !== sectionName && (
-              <h2 className="text-xl">{props.section}</h2>
-            )}
-            <Resource user={user} {...props} />
-          </>
-        )
-        sectionName = props.section
-        return resource
-      })}
-    </div>
+        <Link to="/admin/resources/new">Add Resource</Link>
+      </AdminToolbar>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl">{name}</h1>
+        <p>{description}</p>
+        {resources.map((props: ResourceProps) => {
+          const resource = (
+            <>
+              {props.section !== sectionName && (
+                <h2 className="text-xl">{props.section}</h2>
+              )}
+              <Resource user={user} {...props} />
+            </>
+          )
+          sectionName = props.section
+          return resource
+        })}
+      </div>
+    </>
   )
 }
 
