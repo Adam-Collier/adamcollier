490221342ef30d7bc6f02e6a6e78e0bd0e3bdc84
Adam Collier <adamcollierart@gmail.com>
2022-01-19 19:04:48 +0000

    group padding

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
   const { user } = useAuth()
 
   return (
-    <ul className="flex flex-row justify-around items-center px-4 py-4 space-x-4 rounded-md fixed bottom-4 w-fit shadow-md z-10 bg-gray-200 bg-opacity-70 backdrop-filter backdrop-blur-sm left-1/2 -translate-x-1/2">
+    <ul className="flex flex-row justify-around items-center p-4 space-x-4 rounded-md fixed bottom-4 w-fit shadow-md z-10 bg-gray-200 bg-opacity-70 backdrop-filter backdrop-blur-sm left-1/2 -translate-x-1/2">
       <li>
         <Link to="/">
           <Home />
