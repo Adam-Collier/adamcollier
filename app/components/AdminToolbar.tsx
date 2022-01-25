4cff47f5cdeccf093088e8dd264fb3d7f6d6f42c
Adam Collier <adamcollierart@gmail.com>
2022-01-25 22:49:56 +0000

    transparent on desktop

--- app/components/AdminToolbar.tsx
+++ app/components/AdminToolbar.tsx
@@ -6,7 +6,7 @@ type AdminToolbarProps = {
 const AdminToolbar = ({ user, children }: AdminToolbarProps) => {
   return user ? (
     <div className="fixed top-0 block w-full left-0">
-      <div className="py-2 px-4 space-x-4 flex text-xs bg-zinc-200 bg-opacity-70 backdrop-filter backdrop-blur-sm space-x-1 backdrop-filter backdrop-blur-sm underline dark:bg-zinc-700 dark:bg-opacity-40 dark:text-zinc-200">
+      <div className="py-2 px-4 space-x-4 flex text-xs bg-zinc-200 bg-opacity-70 backdrop-filter backdrop-blur-sm space-x-1 backdrop-filter backdrop-blur-sm underline dark:bg-zinc-700 dark:bg-opacity-40 dark:text-zinc-200 sm:bg-transparent sm:dark:bg-transparent">
         {children}
       </div>
     </div>
