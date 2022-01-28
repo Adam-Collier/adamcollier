d00ac757403690e163537760ada264ab8110bfbb
Adam Collier <adamcollierart@gmail.com>
2022-01-28 18:22:58 +0000

    better layout, order items

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,13 +1,26 @@
-import { json, Link, LoaderFunction, Outlet, useLoaderData } from 'remix'
+import { useEffect } from 'react'
+import {
+  json,
+  Link,
+  LoaderFunction,
+  Outlet,
+  useLoaderData,
+  useLocation,
+} from 'remix'
 import { db } from '~/utils/db.server'
-import { toReadableDate, toSlug } from '~/utils/utils'
+import { copyCodeToClipboard, toReadableDate, toSlug } from '~/utils/utils'
 
 export const loader: LoaderFunction = async () => {
   const data = await db.post.findMany({
     where: { published: true },
+    orderBy: [
+      {
+        createdAt: 'desc',
+      },
+    ],
     select: {
       title: true,
-      updatedAt: true,
+      createdAt: true,
     },
   })
 
@@ -16,26 +29,39 @@ export const loader: LoaderFunction = async () => {
 
 type Post = {
   title: string
-  updatedAt: Date
+  createdAt: Date
 }
 
 const Blog = () => {
   const data = useLoaderData()
+  const location = useLocation()
+
+  const slugClasses = location.pathname.endsWith('blog') ? '' : 'hidden'
+
+  useEffect(() => {
+    copyCodeToClipboard()
+  }, [])
 
   return (
-    <div className="flex flex-col-reverse min-w-0 sm:flex-row sm:max-w-7xl sm:mx-auto gap-8 lg:gap-x-16 py-16 w-full px-4">
-      <ul className="hidden lg:block sm:w-48 sm:flex-shrink-0 space-y-3 sm:self-start sm:sticky sm:top-16">
-        {data.map((post: Post) => (
-          <li className="hover:bg-gray-50 p-2 rounded">
-            <Link to={toSlug(post.title)}>
-              <h2 className="text-sm leading-5 text-gray-700">{post.title}</h2>
-              <small className="text-xs text-gray-400">
-                {toReadableDate(post.updatedAt)}
-              </small>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-col-reverse min-w-0 sm:flex-row sm:max-w-7xl sm:mx-auto gap-8 lg:gap-x-16 w-full px-4 justify-between py-16">
+      <aside
+        className={`${slugClasses} lg:block sm:w-56 sm:flex-shrink-0 sm:self-start sm:sticky sm:top-0 sm:overflow-y-scroll h-screen no-scrollbar sm:-my-16 sm:py-16`}
+      >
+        <ul>
+          {data.map((post: Post) => (
+            <li className="hover:bg-gray-50 p-2 rounded">
+              <Link to={toSlug(post.title)}>
+                <h2 className="text-md sm:text-sm leading-5 text-gray-700">
+                  {post.title}
+                </h2>
+                <small className="text-xs text-gray-400">
+                  {toReadableDate(post.createdAt)}
+                </small>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
       <Outlet />
     </div>
   )
