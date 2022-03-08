a402394db4eca31ca420dcf7ee0e37e77870c1fc
Adam <adamcollierart@gmail.com>
2022-03-08 19:29:14 +0000

    Link prefetch (#28)
    * add prefetch to Links
    
    * handleDataRequest function for prefetch caching

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,32 +17,32 @@ export const Header = () => {
     <div className="flex fixed bottom-8 w-full mx-auto left-1/2 -translate-x-1/2 justify-center px-8 z-10">
       <ul className="flex justify-around items-center p-4 space-x-4 rounded-lg w-fit shadow-md z-10 bg-gray-200 bg-opacity-90 backdrop-filter backdrop-blur-sm">
         <li>
-          <Link to="/" aria-label="Home">
+          <Link prefetch="intent" to="/" aria-label="Home">
             <Home />
           </Link>
         </li>
         <li>
-          <Link to="/blog" aria-label="Blog">
+          <Link prefetch="intent" to="/blog" aria-label="Blog">
             <Posts />
           </Link>
         </li>
         <li>
-          <Link to="/snippets" aria-label="Snippets">
+          <Link prefetch="intent" to="/snippets" aria-label="Snippets">
             <Snippets />
           </Link>
         </li>
         <li>
-          <Link to="/resources" aria-label="Resources">
+          <Link prefetch="intent" to="/resources" aria-label="Resources">
             <Resources />
           </Link>
         </li>
         <li>
-          <Link to="/inspiration" aria-label="Inspiration">
+          <Link prefetch="intent" to="/inspiration" aria-label="Inspiration">
             <Inspiration />
           </Link>
         </li>
         <li>
-          <Link to="/music" aria-label="Music">
+          <Link prefetch="intent" to="/music" aria-label="Music">
             <Music />
           </Link>
         </li>
