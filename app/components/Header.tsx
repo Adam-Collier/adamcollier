88c152a032ee7f697d3d291218a4075c3c0b10c6
Adam <adamcollierart@gmail.com>
2022-03-17 17:43:33 +0000

    show the current active route (#31)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'remix'
+import { Link, LinkProps, useLocation } from 'remix'
 import {
   Home,
   Posts,
@@ -11,6 +11,33 @@ import {
 } from '~/svgs'
 import { useAuth } from '~/context'
 
+const NavLink = (props: LinkProps) => {
+  const location = useLocation()
+
+  const toProp = props.to as string
+
+  const activeClass =
+    // cover all matching cases inc homepage
+    location.pathname === props.to ||
+    // check the length is greater than one to remove the homepage being grouped in
+    // then check if the location starts with the to prop value
+    (toProp.length > 1 && location.pathname.startsWith(toProp as string))
+      ? ''
+      : 'hidden'
+
+  return (
+    <Link {...props} className="relative block">
+      {props.children}
+      <span
+        className={[
+          activeClass,
+          'bg-black absolute -bottom-1.5 left-1/2 -translate-x-1/2 h-1 w-1 rounded-lg',
+        ].join('')}
+      ></span>
+    </Link>
+  )
+}
+
 export const Header = () => {
   const { user } = useAuth()
 
@@ -18,34 +45,34 @@ export const Header = () => {
     <div className="flex fixed bottom-8 w-full mx-auto left-1/2 -translate-x-1/2 justify-center px-8 z-10">
       <ul className="flex justify-around items-center p-4 space-x-3 sm:space-x-4 rounded-lg w-fit shadow-md z-10 bg-gray-200 bg-opacity-90 backdrop-filter backdrop-blur-sm">
         <li>
-          <Link prefetch="intent" to="/" aria-label="Home">
+          <NavLink prefetch="intent" to="/" aria-label="Home">
             <Home />
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link prefetch="intent" to="/blog" aria-label="Blog">
+          <NavLink prefetch="intent" to="/blog" aria-label="Blog">
             <Posts />
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link prefetch="intent" to="/snippets" aria-label="Snippets">
+          <NavLink prefetch="intent" to="/snippets" aria-label="Snippets">
             <Snippets />
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link prefetch="intent" to="/resources" aria-label="Resources">
+          <NavLink prefetch="intent" to="/resources" aria-label="Resources">
             <Resources />
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link prefetch="intent" to="/inspiration" aria-label="Inspiration">
+          <NavLink prefetch="intent" to="/inspiration" aria-label="Inspiration">
             <Inspiration />
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link prefetch="intent" to="/music" aria-label="Music">
+          <NavLink prefetch="intent" to="/music" aria-label="Music">
             <Music />
-          </Link>
+          </NavLink>
         </li>
         <li className="w-[1px] h-[20px] bg-zinc-400"></li>
         {user ? (
@@ -58,7 +85,11 @@ export const Header = () => {
           </li>
         ) : (
           <li>
-            <a href="https://twitter.com/CollierAdam">
+            <a
+              href="https://twitter.com/CollierAdam"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Twitter />
             </a>
           </li>
